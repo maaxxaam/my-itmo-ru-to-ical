69b88c3f3edde6e80041ac895b8534addbb1ca00
maaxxaam <maaxxaam@protonmail.com>
2023-09-11 18:05:58 +0300

    Improve calendar processing
    - Replace None LOCATION with an empty string
    - Added event categories
    - Shorten "Laboratory lessons" to "Lab"

--- src/calendar_processing.py
+++ src/calendar_processing.py
@@ -9,6 +9,7 @@ from ics import Event, Calendar
 _event_type_to_tag_map = {
     "Лекции": "Лек",
     "Практические занятия": "Прак",
+    "Лабораторные занятия": "Лаб",
     "Зачет": "Зачет",
 }
 
@@ -47,18 +48,17 @@ def _event_type_to_tag(t: str):
     return _event_type_to_tag_map.get(t, t)
 
 
-def _raw_event_to_description(re: dict):
+def _raw_event_to_description(re: dict) -> str:
     lines = []
     for key, name in _raw_event_key_names.items():
         if re[key]:
             lines.append(f"{name}: {re[key]}")
 
     _msk_formatted_datetime = (datetime.utcnow() + timedelta(hours=3)).strftime("%Y-%m-%d %H:%M")
-    lines.append(f"Обновлено: {_msk_formatted_datetime} MSK")
     return "\n".join(lines)
 
 
-def _raw_event_to_location(re: dict):
+def _raw_event_to_location(re: dict) -> str:
     elements = []
     for key in "room", "building":
         if re[key]:
@@ -69,19 +69,21 @@ def _raw_event_to_location(re: dict):
     if re["zoom_url"]:
         result = f"Zoom / {result}" if result else "Zoom"
 
-    return result if result else None
+    return result if result else "" 
 
 
 def raw_events_to_calendar(raw_events: List[dict]):
     calendar = Calendar()
     calendar.creator = _CALENDAR_CREATOR_VALUE
     for raw_event in raw_events:
+        event_type: str = _event_type_to_tag(raw_event['type'])
         event = Event(
-            name=f"[{_event_type_to_tag(raw_event['type'])}] {raw_event['subject']}",
+            name=f"[{event_type}] {raw_event['subject']}",
             begin=isoparse(f"{raw_event['date']}T{raw_event['time_start']}:00+03:00"),
             end=isoparse(f"{raw_event['date']}T{raw_event['time_end']}:00+03:00"),
             description=_raw_event_to_description(raw_event),
             location=_raw_event_to_location(raw_event),
+            categories=[event_type]
         )
         if raw_event["zoom_url"]:
             event.url = raw_event["zoom_url"]
