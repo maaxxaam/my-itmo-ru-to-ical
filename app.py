eced6075c4501b6df07fed0ea9a4ae8dae8e58ef
maaxxaam <maaxxaam@protonmail.com>
2023-09-11 18:24:09 +0300

    Finish date type checking code

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ from calendar_processing import get_raw_events, raw_events_to_calendar
 from datetime import date, timedelta
 import argparse
 
-#TODO: read config
 def get_calendar(login, password, start, end, output):
     token = get_access_token(login, password)
     events = get_raw_events(token, start, end)
@@ -15,8 +14,9 @@ def get_calendar(login, password, start, end, output):
 def check_dates(dates):
     try:
         converted_date = date.fromisoformat(dates)
+        return dates
     except Exception:
-        raise ValueError('Date was not supplied with format YYYY-MM-DD')
+        raise argparse.ArgumentTypeError('Date was not supplied with format YYYY-MM-DD')
 
 if __name__ == '__main__':
     today = date.today()
@@ -32,8 +32,10 @@ if __name__ == '__main__':
     parser.add_argument('-o', '--output', default='my.itmo.ics',
                         help='Path to the file to save the calendar to')
     parser.add_argument('-s', '--start', default=def_day_from,
+                        type=check_dates,
                         help='Where to start calendar events from. Provide date as YYYY-MM-DD. Defaults to yesterday.')
     parser.add_argument('-e', '--end', default=def_day_until,
+                        type=check_dates,
                         help='Where to end calendar events. Provide date as YYYY-MM-DD. Defaults to 180 days ahead of today.')
     args = parser.parse_args()
     get_calendar(**vars(args))
